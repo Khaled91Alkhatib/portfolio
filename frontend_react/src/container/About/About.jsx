91a3fa3f20a24a8ab9b1bab07181c5dd4984100e
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-10-19 16:00:03 -0400

    Put all code in a div and modify style

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import "./About.scss";
 
@@ -21,24 +21,30 @@ const About = () => {
 
   return (
     <>
-      <h2 className='head-text'>I know that <span>Good Development</span><br />means <span>Good business</span></h2>
-      <div className='app__profiles'>
-        {abouts.map((about, index) => (
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
-            className="app__profile-item"
-            key={about.title + index}
-          >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
-            <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
-          </motion.div>
-        ))}
+      <div className='about-all'>
+        <h2 className='head-text'>I know that <span>Good Development</span><br />means <span>Good business</span></h2>
+        <div className='app__profiles'>
+          {abouts.map((about, index) => (
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.5, type: 'tween' }}
+              className="app__profile-item"
+              key={about.title + index}
+            >
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
+              <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
+              <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
+            </motion.div>
+          ))}
+        </div>
       </div>
     </>
   );
 };
 
-export default AppWrap(About, 'about');
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  "app__whitebg"
+);
