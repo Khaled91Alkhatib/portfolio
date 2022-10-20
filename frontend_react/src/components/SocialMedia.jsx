e2f329e000c15bef8cd80c2203698821fddb22bf
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-10-20 11:49:50 -0400

    Add routes to buttons

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -5,10 +5,10 @@ const SocialMedia = () => {
   return (
     <div className='app__social'>
       <div>
-        <BsLinkedin />
+        <a href="https://www.linkedin.com/in/khaledalkhatib/" target="_blank" rel="noopener noreferrer"><BsLinkedin /></a>
       </div>
       <div>
-        <BsGithub />
+        <a href="https://github.com/Khaled91Alkhatib" target="_blank" rel="noopener noreferrer"><BsGithub /></a>
       </div>
     </div>
   );
