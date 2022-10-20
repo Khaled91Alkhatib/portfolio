0ee05ea8133c7180b620ad9f6d91aee1b82654fe
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-10-20 10:43:21 -0400

    Export all created schemas

--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -1,8 +1,14 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+import abouts from './abouts';
+import works from './works';
+import skills from './skills';
+import experiences from './experiences';
+import workExperience from './workExperience';
+import contact from './contact';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -11,6 +17,11 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    abouts,
+    works,
+    skills,
+    experiences,
+    workExperience,
+    contact
   ]),
-})
+});
