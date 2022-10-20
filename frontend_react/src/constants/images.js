575359b896573ffe84a6bad08451cad8169b2bbf
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-10-20 10:40:46 -0400

    Export all images

---
+++ frontend_react/src/constants/images.js
@@ -0,0 +1,42 @@
+import email from '../assets/email.png';
+import mobile from '../assets/mobile.png';
+import cpp from '../assets/cpp.png';
+import css from '../assets/css.png';
+import git from '../assets/git.png';
+import graphql from '../assets/graphql.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import mu5 from '../assets/mu5.png';
+import node from '../assets/node.png';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+import typescript from '../assets/typescript.png';
+
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
+import logo from '../assets/logo.png';
+
+const allImages = {
+  email,
+  mobile,
+  cpp,
+  css,
+  git,
+  graphql,
+  html,
+  javascript,
+  mu5,
+  node,
+  python,
+  react,
+  redux,
+  sass,
+  typescript,
+  profile,
+  circle,
+  logo,
+};
+
+export default allImages;
